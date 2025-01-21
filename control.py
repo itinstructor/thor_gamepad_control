920c7a96dcf0d4248c61636ab54708b447016b58
Mike Zubko <mishau7@gmail.com>
2025-01-21 12:39:48 -0700

    control.py change directions

--- control.py
+++ control.py
@@ -24,28 +24,28 @@ while read_thread.is_alive():
         case keys.L1:
             Thread(target=thor.close_claw).start()
         case keys.RIGHT:
-            Thread(target=thor.rotate_claw).start()
-        case keys.LEFT:
             Thread(target=thor.rotate_claw_cw).start()
+        case keys.LEFT:
+            Thread(target=thor.rotate_claw).start()
         case keys.DOWN:
-            Thread(target=thor.move_claw).start()
-        case keys.UP:
             Thread(target=thor.move_claw_b).start()
+        case keys.UP:
+            Thread(target=thor.move_claw).start()
         case keys.A_2:
-            Thread(target=thor.art_4).start()
-        case keys.Y_4:
             Thread(target=thor.art_4_cw).start()
+        case keys.Y_4:
+            Thread(target=thor.art_4).start()
         case keys.B_3:
-            Thread(target=thor.art_3).start()
-        case keys.X_1:
             Thread(target=thor.art_3_b).start()
+        case keys.X_1:
+            Thread(target=thor.art_3).start()
         case keys.R1:
-            Thread(target=thor.art_2).start()
-        case keys.R2:
             Thread(target=thor.art_2_b).start()
+        case keys.R2:
+            Thread(target=thor.art_2).start()
         case keys.L_AN:
-            Thread(target=thor.art_1).start()
-        case keys.R_AN:
             Thread(target=thor.art_1_cw).start()
+        case keys.R_AN:
+            Thread(target=thor.art_1).start()
         case keys.START:
             Thread(target=thor.home).start()
